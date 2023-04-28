307cbb642b10031dc896d785505e91cd5de4cece
ankitvedak <ankitvedak@wpi.edu>
2023-04-28 13:27:18 -0400

    First file updated

--- src/call_from.cpp
+++ src/call_from.cpp
@@ -10,6 +10,14 @@ void call_from(int tid)
 int main()
 {
     // Create an array of 10 thread objects 
-
     std:: thread t[10];
+
+    // Launch 10 threads with intial function "call_from()"
+    for( int i = 0; i < 10; i++)
+    {
+        t[i] = std::thread(call_from, i);
+    }
+
+    // Print from main function
+    std::cout << "Launched from main" << std::endl;
 }
