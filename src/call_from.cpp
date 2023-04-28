5f930e524a3590b5c2e65d556959662afaa1c78b
ankitvedak <ankitvedak@wpi.edu>
2023-04-28 13:45:05 -0400

    Call_from.cpp updated with thread joins

--- src/call_from.cpp
+++ src/call_from.cpp
@@ -20,4 +20,18 @@ int main()
 
     // Print from main function
     std::cout << "Launched from main" << std::endl;
+
+    // wait for the threads to complete
+    for(int i = 0; i < 10; i++)
+    {
+        t[i].join();
+        // std::cout<<"Launched by thread join "<< i <<std::endl;
+    }
+
+    return 0;
 }
+
+/*
+    Key things to note cout is not thread safe.
+    As there is no synchronization currently done on the system there are bunch of cout threads trying to the write at the same time causing a jumbled mess
+*/ 
